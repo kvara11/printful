f49b21101c34ba2baaebe4baf33684203c82e65e
Nika <nikakvaratskhelia11@gmail.com>
2024-06-30 19:59:10 +0400

    add api trait

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits;
+
+
+trait ApiResponses
+{
+
+    protected function success($data, $message = null, $code = 200)
+    {
+        $this->sendResponse([
+            'status' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+
+    protected function error($data, $message = null, $code = 500)
+    {
+        $this->sendResponse([
+            'status' => false,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+
+    private function sendResponse($responseData, $code)
+    {
+        http_response_code($code);
+        header('Content-Type: application/json');
+
+        echo json_encode($responseData);
+        exit;
+    }
+}
