8ce31117a1b155deff321a54efd2f950d581d80e
Nika <nikakvaratskhelia11@gmail.com>
2024-07-01 01:40:11 +0400

    setup app entry point

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-phpinfo();
+require_once realpath("vendor/autoload.php");
+
+use Dotenv\Dotenv;
+use App\Controllers\Controller;
+
+// setup environment variables
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$controller = new Controller();
+
+if ($requestUri == '/' && $requestMethod == 'GET') {
+    $controller->fetchData();
+    
+} else {
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}
